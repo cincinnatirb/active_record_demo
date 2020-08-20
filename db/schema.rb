511c7a6a37a9f637e3221bbdebd0003303cc5dd0
Bill Barnett <bill@gaslight.co>
2020-08-20 17:23:13 -0400

    rails generate scaffold User username:string bio:string && rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_211832) do
+ActiveRecord::Schema.define(version: 2020_08_20_212255) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "username"
     t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
