941f61c36a9fa57865e480659700aaff6a14f42a
Bill Barnett <bill@gaslight.co>
2020-08-20 17:49:52 -0400

    Remove username from Tweet and fix up views

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_213323) do
+ActiveRecord::Schema.define(version: 2020_08_20_213557) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string "username"
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
